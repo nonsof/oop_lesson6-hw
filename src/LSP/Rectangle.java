d4e64856024faab58283e6f20c9d404b18a13773
nonsof <113558928+nonsof@users.noreply.github.com>
2023-10-19 15:44:24 +0300

    Update Rectangle.java

--- src/LSP/Rectangle.java
+++ src/LSP/Rectangle.java
@@ -1,14 +1,19 @@
+
+//Ваш класс Rectangle представляет собой подкласс класса QuadRangle и описывает прямоугольник. Давайте прокомментируем этот код:
+
 package LSP;
 
 public class Rectangle extends QuadRangle {
     private int width;
     private int height;
 
+    // Конструктор класса Rectangle, принимающий ширину и высоту прямоугольника
     public Rectangle(int width, int height) {
         this.width = width;
         this.height = height;
     }
 
+    // Переопределенный метод для расчета площади прямоугольника
     @Override
     public int area() {
         return this.height * this.width;
@@ -21,6 +26,9 @@ public class Rectangle extends QuadRangle {
     public void setWidth(int width) {
         this.width = width;
     }
-
-
 }
+//Конструктор класса Rectangle: Конструктор класса Rectangle принимает ширину и высоту прямоугольника и инициализирует соответствующие поля width и height. Это позволяет создавать объекты Rectangle с указанными размерами.
+
+//Переопределение метода area(): Класс Rectangle переопределяет метод area(), унаследованный от суперкласса QuadRangle. В данной реализации метода area(), площадь прямоугольника вычисляется как произведение ширины на высоту.
+
+//Геттер и сеттер для width: Класс Rectangle предоставляет методы getWidth() и setWidth(), чтобы получить и установить значение ширины прямоугольника. Эти методы обеспечивают доступ к полю width, соблюдая инкапсуляцию данных.
