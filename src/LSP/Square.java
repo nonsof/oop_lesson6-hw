99519bd55a6627a08885e740112770b736c92697
nonsof <113558928+nonsof@users.noreply.github.com>
2023-10-19 15:43:39 +0300

    Update Square.java

--- src/LSP/Square.java
+++ src/LSP/Square.java
@@ -1,12 +1,16 @@
+//код описывает класс Square, который является подклассом класса QuadRangle. Давайте прокомментируем этот код:
+
 package LSP;
 
-public class Square extends QuadRangle{
+public class Square extends QuadRangle {
     private int length;
 
+    // Конструктор класса Square, принимающий длину стороны квадрата
     public Square(int length) {
         this.length = length;
     }
 
+    // Переопределенный метод для расчета площади квадрата
     @Override
     public int area() {
         return this.length * this.length;
@@ -20,3 +24,9 @@ public class Square extends QuadRangle{
         this.length = length;
     }
 }
+
+//Конструктор класса Square: Конструктор класса Square принимает длину стороны квадрата и инициализирует поле length. Это позволяет создавать объекты Square с указанной длиной стороны.
+
+//Переопределение метода area(): Класс Square переопределяет метод area(), унаследованный от суперкласса QuadRangle. В данной реализации метода area(), площадь квадрата вычисляется как произведение длины стороны на саму себя.
+
+//Геттер и сеттер для length: Класс Square предоставляет методы getLength() и setLength(), чтобы получить и установить значение длины стороны квадрата. Эти методы обеспечивают доступ к полю length, соблюдая инкапсуляцию данных.
